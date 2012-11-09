11fe9b0b8f00a64d8c6e88b48ac7d3fec6ef4ec9
Jose-Flavio AGUILAR PAULINO <jose-flavio.aguilarpaulino@amadeus.com>
2012-11-09 18:26:18 +0100

    [dev] improving

--- dajo-types/scripts/reformat.sh
+++ dajo-types/scripts/reformat.sh
@@ -10,3 +10,4 @@ fi
 sed -i 's,\r,,g' $1     #remove windows line breaks
 sed -i 's,\t,    ,g' $1 #remove tabs
 sed -i 's/[ ]*$//g' $1  #remove trailing spaces
+sed -i /./,/^$/!d $1    #remove duplicated empty lines
