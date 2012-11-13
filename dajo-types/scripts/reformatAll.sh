9664c2b4b2ca90497ea12a8d05a0d7ce7dae3449
Jose-Flavio AGUILAR PAULINO <jose-flavio.aguilarpaulino@amadeus.com>
2012-11-13 15:48:34 +0100

    [doc] better docs

--- dajo-types/scripts/reformatAll.sh
+++ dajo-types/scripts/reformatAll.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-NUMBER_OF_CORES=$(nproc)
+#o you can use $(nproc) if available
+NUMBER_OF_CORES=$(cat /proc/cpuinfo| grep processor | wc -l)
 echo number of cores: $NUMBER_OF_CORES
 echo files: $1
 /bin/find . -type f -name $1 | xargs -P $NUMBER_OF_CORES -n 1 reformat.sh
